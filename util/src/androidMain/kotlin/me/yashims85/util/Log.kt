b2b0a7047e8e284f743bf82cd83c0ea7d3949c42
Masaya Yashiro <yashims85@gmail.com>
2019-02-10 15:13:16 +0900

    master feat(util): add log module

---
+++ util/src/androidMain/kotlin/me/yashims85/util/Log.kt
@@ -0,0 +1,17 @@
+package me.yashims85.util
+
+private const val APP_TAG: String = "me.yashims85"
+
+internal actual fun logWrite(msg: String, logLevel: LogLevel) {
+    when (logLevel) {
+        LogLevel.DEBUG -> {
+            android.util.Log.d(APP_TAG, msg)
+        }
+        LogLevel.WARN -> {
+            android.util.Log.w(APP_TAG, msg)
+        }
+        LogLevel.ERROR -> {
+            android.util.Log.e(APP_TAG, msg)
+        }
+    }
+}
---
+++ util/src/commonMain/kotlin/me/yashims85/util/Log.kt
@@ -0,0 +1,16 @@
+package me.yashims85.util
+
+internal enum class LogLevel {
+    DEBUG, WARN, ERROR
+}
+
+internal expect fun logWrite(msg: String, logLevel: LogLevel)
+
+object Log {
+    fun d(msg: String) = logWrite(msg, LogLevel.DEBUG)
+    fun d(exception: Exception) = d(exception.toString())
+    fun w(msg: String) = logWrite(msg, LogLevel.WARN)
+    fun w(exception: Exception) = w(exception.toString())
+    fun e(msg: String) = logWrite(msg, LogLevel.ERROR)
+    fun e(exception: Exception) = e(exception.toString())
+}
